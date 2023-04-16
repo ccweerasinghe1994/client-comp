60f9468cb68721d114f0951381eb640583f36140
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-04-16 11:46:44 +0530

    small changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return (
-    <main className={inter.className} >
-        <h1>My page</h1>
-    </main>
-  )
+	return (
+		<main className={inter.className}>
+			<h1 className="text-white text-4xl md:text-8xl flex justify-center items-center h-screen">
+				Search Any Thing you want
+			</h1>
+		</main>
+	);
 }
